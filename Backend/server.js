c71cd865519506ad3e44a930f06fe0ff76d40215
Ayan Asad <ayan03121015287@gmail.com>
2026-06-25 05:29:30 -0700

    Ready for deployment

--- Backend/server.js
+++ Backend/server.js
@@ -1,17 +1,28 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const dns = require("dns");
 const cors = require("cors");
+
+dns.setDefaultResultOrder("ipv4first");
+require("dotenv").config();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+// Routes
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/cv", require("./routes/cv"));
 
-mongoose.connect("mongodb://localhost:27017/myReume");
+// MongoDB Atlas Connection
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
 
-app.listen(60, () => {
-  console.log("Server running on 60");
+// Server
+const PORT = process.env.PORT || 60;
+
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
 });
